5fc9d2727fdee1c8cf4b2303a2f2085b2cc44faa
leahy089 <leahy089@morris.umn.edu>
2019-12-10 11:31:46 -0600

    Add a result array Made some comments for the future

--- src/segmentedfilesystem/QuoteClient.java
+++ src/segmentedfilesystem/QuoteClient.java
@@ -16,15 +16,24 @@ public class QuoteClient {
             System.out.println("Usage: java QuoteClient <csci-4409.morris.umn.edu>");
             return;
         }*/
+
+        //array for the assembly of the final product (type DatagramPacket is subject to change)
+        ArrayList<DatagramPacket> finalResult = new ArrayList<DatagramPacket>(1);
         try {
+            //socket connection to the server
             DatagramSocket socket = new DatagramSocket();
             address = InetAddress.getByName("csci-4409.morris.umn.edu");
             packet = new DatagramPacket(sendBuf, sendBuf.length, address, port);
             socket.send(packet);
 
+            //eventually we would like to place a while loop (maybe while there are still files not received yet?)
+            //receiving the packet from the server
             byte[] buf = new byte[1028];
             DatagramPacket receivedPacket = new DatagramPacket(buf, buf.length);
             socket.receive(receivedPacket);
+            finalResult.add(receivedPacket);
+
+            //Printing the data received for debugging purposes
             String received = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
             System.out.println(received);
         }
